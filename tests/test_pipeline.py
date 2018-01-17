0842bf41d9ef1f178dc68b9abc6f8166ee6b3156
Robert Hickman <you@example.com>
2018-01-17 13:56:49 +0000

    test pipeline compress and encrypt seperately

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,17 +1,16 @@
 import rrbackup.pipeline as pipeline
 import pysodium, unittest, pprint
 
+def write_helper(data, meta, config): meta['data'] = data; return meta
+def read_helper(meta, config): return meta['data'], meta
+
 class test_pipeline(unittest.TestCase):
     def test_simple_pipeline(self):
-        def write_helper(data, meta, config): meta['data'] = data; return meta
-        def read_helper(meta, config): return meta['data'], meta
-
         config = {'encrypt_opts' : {}, 'stream_crypt_key' : pysodium.crypto_secretstream_xchacha20poly1305_keygen()}
 
         meta_pl_format = pipeline.get_default_pipeline_format()
         meta_pl_format['format'].update({'compress'   : None,
                                          'encrypt'    : config['encrypt_opts']})
-
         data_in = b'some data input'
 
         #-------
@@ -28,4 +27,43 @@ class test_pipeline(unittest.TestCase):
         self.assertEqual(data_in, data_out)
 
 
+    def test_simple_pipeline_compress(self):
+        config = {}
 
+        meta_pl_format = pipeline.get_default_pipeline_format()
+        meta_pl_format['format'].update({'compress'   : None})
+        data_in = b'some data input'
+
+        #-------
+        pl_out = pipeline.build_pipeline(write_helper, 'out')
+        meta = {'path' : 'test', 'header' : pipeline.serialise_pipeline_format(meta_pl_format)}
+        meta2 = pl_out(data_in, meta, config)
+
+        self.assertNotEqual(data_in, meta2['data'])
+
+        #-------
+        pl_in = pipeline.build_pipeline(read_helper, 'in')
+        data_out, meta3 = pl_in(meta2, config)
+
+        self.assertEqual(data_in, data_out)
+
+
+    def test_simple_pipeline_encrypt(self):
+        config = {'encrypt_opts' : {}, 'stream_crypt_key' : pysodium.crypto_secretstream_xchacha20poly1305_keygen()}
+
+        meta_pl_format = pipeline.get_default_pipeline_format()
+        meta_pl_format['format'].update({'encrypt'    : config['encrypt_opts']})
+        data_in = b'some data input'
+
+        #-------
+        pl_out = pipeline.build_pipeline(write_helper, 'out')
+        meta = {'path' : 'test', 'header' : pipeline.serialise_pipeline_format(meta_pl_format)}
+        meta2 = pl_out(data_in, meta, config)
+
+        self.assertNotEqual(data_in, meta2['data'])
+
+        #-------
+        pl_in = pipeline.build_pipeline(read_helper, 'in')
+        data_out, meta3 = pl_in(meta2, config)
+
+        self.assertEqual(data_in, data_out)
